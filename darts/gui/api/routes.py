37032218d855f2a9c9f3d28f4770ff553ab37b2c
Luuk <luukderooij@gmail.com>
2023-04-25 21:14:43 +0200

    Fixing update folder.

--- darts/gui/api/routes.py
+++ darts/gui/api/routes.py
@@ -462,9 +462,7 @@ def uploadfile():
 
         print(f"Path = {settings.DATA_DIR}")
 
-        #file.save(os.path.join(".\\uploads\\", filename))
-        print(os.path.join(os.getcwd(), "uploads", filename))
-        file.save(os.path.join(os.getcwd(), "uploads", filename))
+        file.save(os.path.join(os.getcwd(), upload_folder, filename))
    
     response = make_response('Response')
     return response
