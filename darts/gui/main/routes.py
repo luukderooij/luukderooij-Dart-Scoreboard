6be6f17bb39f32053c584d15b04c14c58cf95935
Luuk <luukderooij@gmail.com>
2024-02-19 16:50:48 +0100

    update

--- darts/gui/main/routes.py
+++ darts/gui/main/routes.py
@@ -12,7 +12,7 @@ from darts.players import Players
 
 logger = logging.getLogger(__name__)
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__,  template_folder='templates', static_folder='static', static_url_path='/main/static')
 
 
 class RegistrationForm(FlaskForm):
